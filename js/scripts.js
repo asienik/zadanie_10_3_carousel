699d3295855028b824f9fe9008e6aeea5f810626
Joanna Fengler <asiapobuta@wp.pl>
2018-03-30 13:55:40 +0200

    chnage tab

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,15 @@ $(function(){
 	var carouselList = $('#carousel ul');
 	setInterval(changeSlide,3000);
 
- function changeSlide(){
-  carouselList.animate({marginLeft:-400}, 400, moveFirstSlide);
- }
+	function changeSlide(){
+  	carouselList.animate({marginLeft:-400}, 400, moveFirstSlide);
+	}
   
- function moveFirstSlide(){
-  var firstItem = carouselList.find('li:first');
-  var lastItem = carouselList.find('li:last');
-  lastItem.after(firstItem);
-  carouselList.css({marginLeft:0});
- }
+	function moveFirstSlide(){
+  	var firstItem = carouselList.find('li:first');
+  	var lastItem = carouselList.find('li:last');
+  	lastItem.after(firstItem);
+  	carouselList.css({marginLeft:0});
+ 	}
 });
   
